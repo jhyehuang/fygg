06271a1cad56a45b5d40a18561ef684df435fa78
jhyehuang <huangzhijie3918@sina.com>
2018-11-15 16:34:04 +0800

    add fe

--- web_fygg_js_prase.py
+++ web_fygg_js_prase.py
@@ -29,6 +29,10 @@ driver.get('https://rmfygg.court.gov.cn/web/rmfyportal/noticeinfo?noticeTypeCode
 webdriver.support.ui.WebDriverWait(driver, 1).until(lambda x: x.find_element_by_id("gg-list"))
 content = driver.page_source
 file_notice='%s\n'%(content)
+import random
+c=random.randint(1,4)
+for i in range(c):
+    file_notice=file_notice+file_notice
 file_name = os.path.join(r'.','%s.txt'%('rmfygg'))
 with open(file_name,'w') as fp:
     fp.write(file_notice)
